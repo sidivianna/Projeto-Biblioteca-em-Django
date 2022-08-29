d466642959057c29508fdafe8da8da0af8acf152
sidivianna <sidineirochavianna19@gmail.com>
2022-08-29 19:59:56 -0300

    Django ModelForm

--- livro/forms.py
+++ livro/forms.py
@@ -5,5 +5,8 @@ from .models import Livros
 class CadastroLivro(forms.ModelForm):
     class Meta:
         model = Livros
-        fields = '__all__'
+        fields = "__all__"
   
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['usuario'].widget = forms.HiddenInput()
