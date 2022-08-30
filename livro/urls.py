d3a25b4b4bada0ab96c212d3b94d7c6da517f759
sidivianna <sidineirochavianna19@gmail.com>
2022-08-30 19:23:04 -0300

    cadastro de categorias e botao de excluir

--- livro/urls.py
+++ livro/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('home/', views.home, name = 'home'),
     path('ver_livro/<int:id>', views.ver_livros, name='ver_livros'),#url dinamica
-    path('cadastrar_livro', views.cadastrar_livro, name='cadastrar_livro')
+    path('cadastrar_livro', views.cadastrar_livro, name='cadastrar_livro'),
+    path('excluir_livro/<int:id>', views.excluir_livro, name='excluir_livro'),
+    path('cadastrar_categoria/', views.cadastrar_categoria, name='cadastrar_categoria')
+
 ]
--- livro/views.py
+++ livro/views.py
@@ -1,26 +1,29 @@
 from datetime import date, datetime
+import http
 from urllib import response
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from usuarios.models import Usuario
 from .models import Emprestimos, Livros, Categoria
 from django import forms
-from .forms import CadastroLivro
+from .forms import CadastroLivro, CategoriaLivro
 
 def home(request):
     if request.session.get('usuario'):
         usuario = Usuario.objects.get(id = request.session['usuario'])
+        status_categoria = request.GET.get('cadastro_categoria')
         livros = Livros.objects.filter(usuario = usuario)
         form = CadastroLivro()
         form.fields['usuario'].initial = request.session['usuario']
         form.fields['categoria'].queryset = Categoria.objects.filter(usuario = usuario)
 
-        return render(request, 'home.html', {'livros': livros, 'usuario_logado': request.session.get('usuario'), 'form': form})
+        form_categoria = CategoriaLivro()
+
+        return render(request, 'home.html', {'livros': livros, 'usuario_logado': request.session.get('usuario'), 'form': form, 'status_categoria': status_categoria, 'form_categoria': form_categoria})
     
     else:
         return redirect('/auth/login/?status=2')
     
-
 def ver_livros(request, id):
     if request.session.get('usuario'):
         livros = Livros.objects.get(id = id)
@@ -30,10 +33,13 @@ def ver_livros(request, id):
             form = CadastroLivro()
             form.fields['usuario'].initial = request.session['usuario']
             
+            form_categoria = CategoriaLivro()
 
             return render(request, 'ver_livro.html', {'livro': livros,     
                                                       'categoria_livro': categoria_livro, 'emprestimos': emprestimos,
-                                                      'usuario_logado':request.session.get('usuario'), 'form': form})
+                                                      'usuario_logado':request.session.get('usuario'), 'form': form,
+                                                      'id_livro': id,
+                                                      'form_categoria': form_categoria})
             
             # nao vai permitir que o usuário acesse direto pela barra de navegação.
         else:
@@ -50,6 +56,22 @@ def cadastrar_livro(request):
         else:
             return HttpResponse('Dados inválidos')
             
+def excluir_livro(request, id):
+    livro = Livros.objects.get(id = id).delete()
+    return redirect('/livro/home')
 
+def cadastrar_categoria(request):
+    form = CategoriaLivro(request.POST)
+    nome = form.data['nome']
+    descricao = form.data['descricao']
+    id_usuario = request.POST.get('usuario')
+    
+    if int(id_usuario) == int(request.session.get('usuario')):
+        user = Usuario.objects.get(id = id_usuario)
+        categoria = Categoria(nome = nome, descricao = descricao, usuario = user)
+        categoria.save()
+        return redirect('/livro/home?cadastro_categoria=1')
+    else:
+        return HttpResponse('Pare de ser malandro. Não foi desta vez.')
 
     
