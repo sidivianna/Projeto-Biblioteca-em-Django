7915d981b22f6dab7c40bcacbbeade9f245fa633
sidivianna <sidineirochavianna19@gmail.com>
2022-09-01 10:40:23 -0300

    Avalicação de qualidade

--- livro/urls.py
+++ livro/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('ver_livro/<int:id>', views.ver_livros, name='ver_livros'),#url dinamica
     path('cadastrar_livro', views.cadastrar_livro, name='cadastrar_livro'),
     path('excluir_livro/<int:id>', views.excluir_livro, name='excluir_livro'),
-    path('cadastrar_categoria/', views.cadastrar_categoria, name='cadastrar_categoria')
+    path('cadastrar_categoria/', views.cadastrar_categoria, name='cadastrar_categoria'),
+    path('cadastrar_emprestimo', views.cadastrar_emprestimo, name='cadastrar_emprestimo'),
 
 ]
