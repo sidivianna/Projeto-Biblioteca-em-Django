f5a5a7f33e5b290aa1d1a3e711a461f4e3ec2754
sidivianna <sidineirochavianna19@gmail.com>
2022-10-17 06:44:27 -0300

    Devolução de livros

--- livro/templatetags/filtros.py
+++ livro/templatetags/filtros.py
@@ -1,8 +1,23 @@
 from django import template
+from datetime import datetime
 
 register = template.Library()
 
 @register.filter
 def mostra_duracao(value1, value2):
-    return (value1 - value2).days
+    if all((isinstance(value1, datetime), isinstance(value2, datetime))):
+        dias = (value1 - value2).days
+        texto = 'Dias'
+        if dias == 1:
+            texto = 'Dia'
+        return f"{dias} {texto}."
+
+    return "Ainda não foi devolvido."
+
+'''@register.filter
+def mostra_duracao(value1, value2):
+    if isinstance(value1, datetime) and isinstance(value2, datetime):
+        return (value1 - value2).days
+
+    return "Ainda não foi devolvido."'''
  
